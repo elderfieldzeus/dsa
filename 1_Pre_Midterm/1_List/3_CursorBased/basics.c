def0b7a57af02cb8b61359febfc60d7a3612e415
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-08-04 23:54:48 +0800

    added Cursor Based implementation basics

---
+++ 1_Pre_Midterm/1_List/3_CursorBased/basics.c
@@ -0,0 +1,174 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define VHSIZE 0XA
+
+typedef struct {
+    int data;
+} Element;
+
+typedef struct {
+    Element elem;
+    int next;
+} Node;
+
+typedef struct {
+    Node VHNode[VHSIZE];
+    int avail;
+} VHeap;
+
+typedef int List;
+
+// Initialize, insert, delete, member, locate, and other operations
+void init(List *L);
+void read(List L, VHeap V);
+void insertFirst(List *L, VHeap *V, int data);
+void insertLast(List *L, VHeap *V, int data);
+void delete(List *L, VHeap *V, int data);
+bool member(List L, VHeap V, int data);
+int locate(List L, VHeap V, int data);
+
+//CursorBased Functions
+void initVH(VHeap *V);
+int mallocVH(VHeap *V);
+void freeVH(VHeap *V, int index);
+
+//for debugging only
+void readNexts(VHeap V);
+
+int main() {
+    VHeap V;
+    List L;
+
+    printf("\033[H\033[J");
+
+    initVH(&V);
+    readNexts(V); //debug
+
+    init(&L);
+    read(L, V);
+
+    insertFirst(&L, &V, 1);
+    read(L, V);
+
+    insertLast(&L, &V, 2);
+    read(L, V);
+
+    delete(&L, &V, 1);
+    read(L, V);
+
+    printf("%s\n", member(L, V, 1) ? "TRUE" : "FALSE");
+
+    printf("%d\n", locate(L, V, 2));
+
+    return 0;
+}
+
+void init(List *L) {
+    *L = -1;
+}
+
+void read(List L, VHeap V) {
+    printf("CB List: ");
+
+    for(int i = L; i != -1; i = V.VHNode[i].next) {
+        printf("%d%s", V.VHNode[i].elem.data, (V.VHNode[i].next == -1) ? ".\n" : ", ");
+    }
+
+    if(L == -1) {
+        printf("EMPTY\n");
+    }
+}
+
+void insertFirst(List *L, VHeap *V, int data) {
+    if(V->avail != -1) {
+        int avail = mallocVH(V);
+        if(avail != -1) {
+            V->VHNode[avail].elem.data = data;
+            V->VHNode[avail].next = *L;
+            *L = avail;
+        }
+    }
+}
+
+void insertLast(List *L, VHeap *V, int data) {
+    if(V->avail != -1) {
+        int *curr;
+
+        for(curr = L; (*curr) != -1; curr = &(V->VHNode[*curr].next)) {}
+
+        int avail = mallocVH(V);
+        if(avail != -1) {
+            V->VHNode[avail].elem.data = data;
+            V->VHNode[avail].next = -1;
+
+            
+            *curr = avail;
+        }
+
+    }
+}
+
+void delete(List *L, VHeap *V, int data) {
+    int *curr;
+    for(curr = L; *curr != -1 && V->VHNode[*curr].elem.data != data; curr = &V->VHNode[*curr].next) {}
+
+    if(*curr != -1) {
+        int temp = *curr;
+        *curr = V->VHNode[*curr].next;
+        freeVH(V, temp);
+    }
+}
+
+bool member(List L, VHeap V, int data) {
+    int curr;
+
+    for(curr = L; curr != -1 && V.VHNode[curr].elem.data != data; curr = V.VHNode[curr].next) {}
+    
+    return (curr == -1) ? false : true;
+}
+
+int locate(List L, VHeap V, int data) {
+    int curr;
+
+    for(curr = L; curr != -1 && V.VHNode[curr].elem.data != data; curr = V.VHNode[curr].next) {}
+    
+    return curr;
+}
+
+//CURSOR BASED FUNCTIONS
+
+void initVH(VHeap *V) {
+    V->avail = 0;
+
+    for(int i = 0; i < VHSIZE; i++) {
+        V->VHNode[i].next = i + 1;
+    }
+
+    V->VHNode[VHSIZE - 1].next = -1;
+}
+
+int mallocVH(VHeap *V) {
+    int temp = -1;
+
+    if(V->avail != -1) {
+        temp = V->avail;
+        V->avail = V->VHNode[temp].next;
+    }
+
+    return temp;
+}
+
+void freeVH(VHeap *V, int index) {
+    if(index != -1) {
+        V->VHNode[index].next = V->avail;
+        V->avail = index;
+    }
+}
+
+void readNexts(VHeap V) {
+    printf("Nexts: ");
+    for(int i = 0; i < VHSIZE; i++) {
+        printf("%d%s", V.VHNode[i].next, (V.VHNode[i].next == -1) ? ".\n" : ", ");
+    }
+}
