0215c2c067cb0c5ec0c899666b6eb5fee2a61376
Zeus D. Elderfield <143060404+elderfieldzeus@users.noreply.github.com>
2024-11-12 08:55:50 +0000

    feat: added layout for listToMaxHeap

---
+++ 3_PreFinal/3_PriorityQueue/3_MaxHeap/listToMaxHeap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
+
+typedef struct {
+    int data[MAX];
+    int lastNdx;
+} HeapList;
+
+void covertToMaxHeap(HeapList *H);
+
+int main() {
+    return 0;
+}
+
+void covertToMaxHeap(HeapList *H) {
+
+}
