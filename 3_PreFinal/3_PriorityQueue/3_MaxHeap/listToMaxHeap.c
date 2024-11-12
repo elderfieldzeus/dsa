79821eb9e1baa96e555f2573ddb72c5399bff8ba
Zeus D. Elderfield <143060404+elderfieldzeus@users.noreply.github.com>
2024-11-12 09:29:19 +0000

    feat: added answer for DSA activity sir gran

--- 3_PreFinal/3_PriorityQueue/3_MaxHeap/listToMaxHeap.c
+++ 3_PreFinal/3_PriorityQueue/3_MaxHeap/listToMaxHeap.c
@@ -7,12 +7,40 @@ typedef struct {
     int lastNdx;
 } HeapList;
 
-void covertToMaxHeap(HeapList *H);
+void listToHeap(HeapList *H);
 
 int main() {
     return 0;
 }
 
-void covertToMaxHeap(HeapList *H) {
+void listToHeap(HeapList *H) {
+    int i = (H->lastNdx - 1) / 2;
 
+    for(; i >= 0; i--) {
+        int parent = i;
+        int largest = parent;
+
+        while(largest == parent) {
+            int Lchild = parent * 2 + 1;
+            int Rchild = Lchild + 1;
+
+            if(Lchild <= H->lastNdx && H->data[largest] < H->data[Lchild]) {
+                largest = Lchild;
+            }
+
+            if(Rchild <= H->lastNdx && H->data[largest] < H->data[Rchild]) {
+                largest = Rchild;
+            }
+
+            if(largest != parent) {
+                int temp = H->data[parent];
+                H->data[parent] = H->data[largest];
+                H->data[largest] = temp;
+                parent = largest;
+            }
+            else {
+                largest = -1; // flag
+            }
+        }
+    }
 }
